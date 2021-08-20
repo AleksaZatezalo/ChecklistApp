44c5ec34fd7a98e58e5bf87c9f4960e3b5f3b615
Aleksa <zabumaphu@gmail.com>
2021-08-20 15:35:28 -0400

    ToDo Adapter created.

--- app/src/main/java/com/example/checklist/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/checklist/Adapter/ToDoAdapter.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
-    private List<ToDoModel>;
+    private List<ToDoModel> todoList;
     private MainActivity activity;
 
     public ToDoAdapter(MainActivity activity){
@@ -31,6 +31,27 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
                 .inflate(R.layout.task_layout, parent, false);
         return new ViewHolder(itemView);
         }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);    }
+
+    public void onBindViewHolder(ViewHolder holder, int position){
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    public int getItemCount(){
+        return todoList.size();
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         CheckBox task;
