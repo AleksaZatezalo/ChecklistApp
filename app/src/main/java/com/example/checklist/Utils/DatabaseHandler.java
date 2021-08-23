1a11a8705432278bafc32039d5fd3f8d23263635
Aleksa <zabumaphu@gmail.com>
2021-08-23 15:45:30 -0400

    Database constants added for SQLite. Database helper instantiated.

--- app/src/main/java/com/example/checklist/Utils/DatabaseHandler.java
+++ app/src/main/java/com/example/checklist/Utils/DatabaseHandler.java
@@ -1,4 +1,17 @@
 package com.example.checklist.Utils;
 
-public class DatabaseHandler {
-}
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String NAME = "toDoListDatabase";
+    private static final String TODO_TABLE = "todo";
+    private static final String ID = "id";
+    private static final String TASK = "task";
+    private static final String STATUS = "status";
+    private static final String CREATE_TODO_TABLE = "CREATE TABLE " + TODO_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TASK + " TEXT, "
+            + STATUS + " INTEGER)";
+
+    private SQLiteDatabase db;}
